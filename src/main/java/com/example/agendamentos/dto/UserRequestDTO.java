11863f59eceba1544882a2cd8fada386c50b6d48
Frouzin <matheusereis44@gmail.com>
2025-05-31 17:10:05 -0300

    feat: DTOs Created

---
+++ src/main/java/com/example/agendamentos/dto/UserRequestDTO.java
@@ -0,0 +1,41 @@
+package com.example.agendamentos.dto;
+
+import com.example.agendamentos.entity.TipoUsuario;
+
+public class UserRequestDTO {
+    private String name;
+    private String email;
+    private String senha;
+    private TipoUsuario tipoUsuario;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public TipoUsuario getTipoUsuario() {
+        return tipoUsuario;
+    }
+    public void setTipoUsuario(TipoUsuario tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+}
