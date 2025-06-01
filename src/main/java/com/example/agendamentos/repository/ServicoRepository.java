1b71e7babaf6af7a37e0f67390eab266081d402d
Frouzin <matheusereis44@gmail.com>
2025-06-01 01:01:22 -0300

    feat: Repository com função de procurar Tipo de Profissional

---
+++ src/main/java/com/example/agendamentos/repository/ServicoRepository.java
@@ -0,0 +1,11 @@
+package com.example.agendamentos.repository;
+
+import com.example.agendamentos.entity.Servico;
+import com.example.agendamentos.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ServicoRepository extends JpaRepository<Servico, Long> {
+    List<Servico> findByProfissional(User profissional);
+}
